2b518eef4c619928e1c11acb0c1dc507ce9738f0
Igor Kashchenko <igorkashchenko.dev@gmail.com>
2023-10-13 02:55:57 +0300

    loading and error handling was implemented

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,29 @@ import { onMounted, ref } from "vue";
 import { Image } from "./types/Image";
 import { fetchImages } from "./utils/fetchImages";
 import ImgList from "./components/ImgList.vue";
+import ProgressSpinner from "primevue/progressspinner";
 
 const images = ref<Image[]>([]);
 const selectedImages = ref<Image[]>([]);
 const selectedImagesId = ref<string[]>([]);
+const isLoading = ref(false);
+const isError = ref(false);
+const errorMsg = ref("");
 
 onMounted(async () => {
   try {
+    isError.value = false;
+    isLoading.value = true;
     const response = await fetchImages();
 
     images.value = response;
   } catch (error) {
+    isError.value = true;
+    isLoading.value = false;
+    errorMsg.value = 'Error fetching images! Try again later!'
     console.log(error);
+  } finally {
+    isLoading.value = false;
   }
 });
 
@@ -33,9 +44,7 @@ const handleSelectImage = (id: string) => {
 
 const handleUnselectImage = (id: string) => {
   selectedImages.value = selectedImages.value.filter((img) => img.id !== id);
-  selectedImagesId.value = selectedImagesId.value.filter(
-    (imgId) => imgId !== id
-  );
+  selectedImagesId.value = selectedImagesId.value.filter((imgId) => imgId !== id);
 };
 </script>
 
@@ -45,15 +54,27 @@ const handleUnselectImage = (id: string) => {
 
     <ImageCarousel
       :images="images"
-      v-if="images.length > 0"
+      v-if="images.length > 0 && !isLoading"
       @selectImage="handleSelectImage"
       @unselect="handleUnselectImage"
       :selectedImagesId="selectedImagesId"
     />
 
-    <p class="text-6xl m-0 text-center mb-4" v-if="selectedImages.length !== 0">Selected Images</p>
+    <div v-if="isLoading && !isError" class="flex flex-grow justify-content-center align-items center">
+      <ProgressSpinner />
+    </div>
 
-    <ImgList :selectedImages="selectedImages" @unselect="handleUnselectImage" />
+      <p v-if="isError" class="text-red-600 text-4xl text-center">{{ errorMsg }}</p>
+
+    <p class="text-6xl m-0 text-center mb-4" v-if="selectedImages.length !== 0">
+      Selected Images
+    </p>
+
+    <ImgList
+      :selectedImages="selectedImages"
+      @unselect="handleUnselectImage"
+      v-if="selectedImages.length !== 0"
+    />
   </AppContainer>
 </template>
 
